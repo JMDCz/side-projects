22222ee685c738ccdf41166222d6fca9c1083294
John Marc Dela Cruz <jmarcdcruz@gmail.com>
2024-01-31 23:20:40 -0500

    Completed simple colour code, random Hex colour code to be completed

--- colour-picker/main.js
+++ colour-picker/main.js
@@ -1,3 +1,17 @@
 //Simple Array to holder 10 colours
 
-let simpleColours = [];
+let colours = ["green", "red", "rgba(133,122,200)", "#F15025"];
+
+const btn = document.getElementById('btn');
+const colour = document.querySelector('.colour');
+
+btn.addEventListener('click', function() {
+    // get a random number between 0 -3
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colours[randomNumber];
+    colour.textContent = colours[randomNumber];
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colours.length);
+}
