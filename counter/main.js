4123ed5ebc376792017a137b968d95a1bcd5599c
John Marc Dela Cruz <jmarcdcruz@gmail.com>
2024-02-07 22:16:12 -0500

    updates to code

---
+++ counter/main.js
@@ -0,0 +1,9 @@
+//Increase 
+document.querySelector('.increase').addEventListener('click', increase());
+
+function increase() {
+    //define value holder variable
+    //increment a variable by 1 each time we increment
+    //use variable as the innerText for .incrementValue
+    document.querySelector('.incrementValue').innerText = 'Test'
+}
