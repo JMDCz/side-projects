d6732ee7093bfcf4be10d33b6039b24fb5db9001
John Marc Dela Cruz <jmarcdcruz@gmail.com>
2024-02-11 18:41:33 -0500

    pushing coding for working counter functionality

--- counter/main.js
+++ counter/main.js
@@ -1,9 +1,25 @@
 //Increase 
-document.querySelector('.increase').addEventListener('click', increase());
+document.querySelector('.increase').addEventListener('click', increase);
+document.querySelector('.reset').addEventListener('click', reset);
+document.querySelector('.decrease').addEventListener('click', decrease);
+
+let counter = 0
 
 function increase() {
     //define value holder variable
     //increment a variable by 1 each time we increment
     //use variable as the innerText for .incrementValue
-    document.querySelector('.incrementValue').innerText = 'Test'
+    counter++;
+    document.querySelector('.incrementValue').innerText = counter;
 }
+
+function reset() {
+    let resetValue = 0
+    counter = resetValue
+    document.querySelector('.incrementValue').innerText = resetValue
+}
+
+function decrease() {
+    counter--;
+    document.querySelector('.incrementValue').innerText = counter
+}s
