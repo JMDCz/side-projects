d4f86d9d26d8092b9b249e30cb6ef5797a8bf835
John Marc Dela Cruz <jmarcdcruz@gmail.com>
2024-02-14 23:45:45 -0500

    adding JS code for preview review view

--- slideshow-reviews/main.js
+++ slideshow-reviews/main.js
@@ -18,9 +18,17 @@ let Nathan = new Reviews('Nathan', 'Product Owner', 'Venenatis a condimentum vit
 
 let Mary = new Reviews('Mary', 'Engineer', 'Aenean et tortor at risus viverra adipiscing at in tellus. Ac auctor augue mauris augue neque gravida in fermentum et. Parturient montes nascetur ridiculus mus. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam purus. Ut ornare lectus sit amet est. Neque laoreet suspendisse interdum consectetur libero id faucibus. Vestibulum mattis ullamcorper velit sed ullamcorper.')
 
-let reviewsArray = [Mark, Nathan, Mary];
+let Robert = new Reviews('Robert', 'Project Manager', 'Venenatis a condimentum vitae sapien pellentesque habitant. Suscipit tellus mauris a diam maecenas. At quis risus sed vulputate odio ut. Viverra aliquet eget sit amet tellus cras adipiscing. In ante metus dictum at tempor commodo. A diam sollicitudin tempor id eu nisl nunc. ')
 
-document.getElementById('viewNextReview').addEventListener('click', nextReview);
+let Sarah = new Reviews('Sarah', 'Technical Consultant', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.')
+
+let reviewsArray = [Mark, Nathan, Mary, Robert, Sarah];
+
+const nextReviewBtn = document.getElementById('viewNextReview');
+const previousReviewBtn = document.getElementById('viewPreviousReview');
+
+nextReviewBtn.addEventListener('click', nextReview);
+previousReviewBtn.addEventListener('click', previousReview);
 
 //Global counter variable for Next Review
 let nextReviewIndex = 0
@@ -39,3 +47,11 @@ function nextReview() {
 
 }
 
+function previousReview() {
+    if(nextReviewIndex < reviewsArray.length) {
+        document.querySelector('.customerName').innerText = reviewsArray[nextReviewIndex].name
+        document.querySelector('.customerRole').innerText = reviewsArray[nextReviewIndex].role
+        document.querySelector('.customerReview').innerText = reviewsArray[nextReviewIndex].review
+        nextReviewIndex--
+        }
+}
